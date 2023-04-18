860af135a11a4d191c606cc8e52c813f7ae02b12
Jonathan Ly <jly02@cs.washington.edu>
2023-04-18 11:59:03 -0700

    Write initial tests for pre decorator

--- tests/test_invariant.py
+++ tests/test_invariant.py
@@ -1,5 +1,33 @@
+import unittest
+
 from invariant import __version__
+from invariant.invariant import pre
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def testVersion():
+    assert __version__ == "0.1.0"
+
+
+@pre(lambda x: x > 0, lambda y: y > 0)
+def add(x: int, y: int):
+    return x + y
+
+
+class TestPre(unittest.TestCase):
+    """Collection of tests to verify precondition
+    decorator works as intended.
+    """
+
+    def testNoFailedCondition(self):
+        add(1, 1)
+
+    def testSingleCondition(self):
+        self.assertRaises(ValueError, add, -1, 1)
+
+    def testSecondCondition(self):
+        self.assertRaises(ValueError, add, 1, -1)
+
+
+if __name__ == "__main__":
+    testVersion()
+    unittest.main()
